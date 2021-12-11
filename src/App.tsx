549f85b7aed2e7f8e0b75d20779ec6cf1f168362
Pedro Antônio <gnx@tuta.io>
2021-12-11 10:33:03 -0300

    aula 2 - maximum speed

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
-import { Button } from './components/Button'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import { Home } from './pages/Home';
+import { NewRoom } from './pages/NewRoom';
+
+import './styles/global.scss'
+
+import { AuthContextProvider } from './contexts/AuthContext'
+
 
 function App() {
+
+
   return (
-    <>
-      <h1>hello world</h1>
-      <Button>Teste galera</Button>
-    </>
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/rooms/new" element={<NewRoom />}/>
+        </Routes>
+        </AuthContextProvider>
+    </BrowserRouter>
   );
 }
 
